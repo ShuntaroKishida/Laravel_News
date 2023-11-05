e8700a652e935255bc70339ee73d82b39237f4b7
ShuntaroKishida <137754393+ShuntaroKishida@users.noreply.github.com>
2023-11-05 15:00:25 +0900

    とりあえずコミット

--- index.php
+++ index.php
@@ -1,52 +1,70 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>シンプルなツイート投稿アプリ</title>
-    <link rel="stylesheet" href="style.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Laravel News</title>
+  <script>
+    function confirmSubmit() {
+      return confirm("投稿しますか？");
+    }
+  </script>
 </head>
+
 <body>
 
-<h1>ツイート投稿アプリ</h1>
+  <div class="container">
+    <h1>Laravel News</h1>
 
-<!-- ツイート投稿フォーム -->
-<form method="post" action="index.php">
-    <label for="tweet">ツイート内容:</label>
-    <input type="text" id="tweet" name="tweet" required>
-    <button type="submit">投稿</button>
-</form>
+    <!-- ツイート投稿フォーム -->
+    <form method="post" action="index.php" onsubmit="return confirmSubmit();">
+      <label for="title">タイトル (最大30文字):</label>
+      <input type="text" id="title" name="title" maxlength="30" required><br>
+      <label for="tweet">ツイート内容:</label>
+      <input type="text" id="tweet" name="tweet" required><br>
+      <button type="submit">投稿</button>
+    </form>
 
-<?php
-// ツイートをテキストファイルに保存
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $tweet = $_POST["tweet"];
-    $file = "tweets.txt";
+    <?php
+    // ツイートをテキストファイルに保存
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      $title = $_POST["title"];
+      $tweet = $_POST["tweet"];
+      $file = "tweets.txt";
 
-    // ファイルにツイートを追記
-    $currentTweets = file_get_contents($file);
-    $currentTweets .= $tweet . "\n";
-    file_put_contents($file, $currentTweets);
-
-    echo "<p>ツイートが保存されました。</p>";
-}
-
-// ツイート一覧を表示
-$file = "tweets.txt";
-if (file_exists($file)) {
-    echo "<h2>ツイート一覧</h2>";
-    $tweets = file_get_contents($file);
-    $tweetArray = explode("\n", $tweets);
-
-    foreach ($tweetArray as $tweet) {
-        if (!empty($tweet)) {
-            echo "<p>" . $tweet . "</p>";
-        }
+      // ファイルにツイートを追記
+      $currentTweets = file_get_contents($file);
+      $currentTweets .= "タイトル: " . $title . "\n" . "ツイート: " . $tweet . "\n\n";
+      file_put_contents($file, $currentTweets);
     }
-} else {
-    echo "<p>まだツイートはありません。</p>";
-}
-?>
+
+    // ツイート一覧を表示
+    $file = "tweets.txt";
+    if (file_exists($file)) {
+      echo "<h2>ツイート一覧</h2>";
+      $tweets = file_get_contents($file);
+      $tweetArray = explode("\n\n", $tweets);
+
+      foreach ($tweetArray as $tweet) {
+        if (!empty($tweet)) {
+          // タイトルとツイート内容を分割
+          list($title, $content) = explode("\n", $tweet, 2);
+          echo "<div class='tweet-container'>";
+          // タイトルをリンクとして表示
+          echo "<a href='tweet_detail.php?title=" . urlencode($title) . "'><strong>" . htmlspecialchars($title) . "</strong></a><br>";
+          echo nl2br(htmlspecialchars($content));
+          echo "</div>";
+          echo "<hr>";  // 横線を追加
+        }
+      }
+    } else {
+      echo "<p>まだツイートはありません。</p>";
+    }
+    ?>
+
+  </div>
 
 </body>
+
 </html>
